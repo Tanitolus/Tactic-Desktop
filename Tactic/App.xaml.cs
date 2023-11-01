6c1510245253a493d321a90c400b2fcac3346a09
Tanitolus <129631204+Tanitolus@users.noreply.github.com>
2023-11-01 22:09:48 +0000

    First

---
+++ Tactic/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Tactic
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
